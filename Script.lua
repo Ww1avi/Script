f8c661ca7093c4dfc250557f6d301c43afff70b9
Ww1avi <kandonmezmomo@gmail.com>
2025-03-26 12:18:23 +0100

    Script.lua

--- Script.lua
+++ Script.lua
@@ -259,3 +259,200 @@ AnimationPack(game.Players.LocalPlayer.Character)
 game.Players.LocalPlayer.CharacterAdded:Connect(AnimationPack)
    end,
 })
+
+local Button = Tab:CreateButton({
+   Name = "Anti-Bag",
+   Callback = function()
+   if game.PlaceId == 2788229376 then
+    local LP = game.Players.LocalPlayer;
+    for i,v in ipairs(LP.Character:GetDescendants()) do
+        if v.Name == "Christmas_Sock" then v:Destroy()
+        end;
+    end;
+    LP.Character.ChildAdded:Connect(function()
+        wait(0.5)
+        for i,v in ipairs(LP.Character:GetDescendants()) do
+            if v.Name == "Christmas_Sock" then v:Destroy()
+            end;
+        end;
+    end);
+end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Chat-Logs",
+   Callback = function()
+   local ChatLogs = true
+
+if game.PlaceId == 2788229376 then
+    if ChatLogs == true then
+        game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Position = game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Position + UDim2.new(UDim.new(),game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Size.Y)
+        game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = true
+    end
+
+    if ChatLogs == false then
+        game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Position = UDim2.new(0, 0, 0, 0)
+        game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatChannelParentFrame.Visible = false
+    end
+end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Infinite Stamina",
+   Callback = function()
+   local LP = game.Players.LocalPlayer
+
+function InfStamina(Character)
+    Character:WaitForChild'BodyEffects'
+    game:GetService('RunService').Stepped:Connect(function()
+        pcall(function()
+            LP.Character.BodyEffects.Movement:ClearAllChildren()
+            LP.Character.BodyEffects.Reload.Value = false
+            LP.Character.BodyEffects.Defense.Value = 9e9
+        end)
+    end)
+end
+InfStamina(game.Players.LocalPlayer.Character)
+game.Players.LocalPlayer.CharacterAdded:Connect(InfStamina)
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Speed Glitch",
+   Callback = function()
+   if game.PlaceId == 2788229376 then
+    setfpscap(360)
+
+    local Notification = true
+    local AutoMacro = true
+    local Macro = true
+    local AutoMacroKey = Enum.KeyCode.Z
+    local MacroKey = Enum.KeyCode.X
+
+    if AutoMacro == true then
+        game:GetService("UserInputService").InputBegan:Connect(function(input,chatting)
+            if input.UserInputType == Enum.UserInputType.Keyboard then
+                if chatting then IsChatting = true else IsChatting = false end
+                if input.KeyCode == AutoMacroKey then
+                    if IsChatting ~= false then return end
+                    if not AutoMacroToggle then
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "AutoMacro";
+                                Text = "AutoMacro: On";
+                                Duration = 3;
+                            })
+                        end
+                        AutoMacroToggle = true
+                        Instance.new("Animation", game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations")).Name = 'Greet'
+                        game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet").AnimationId = 'rbxassetid://3189777795'
+                        local Greet = game.ReplicatedStorage.ClientAnimations.Greet
+                        local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+                        local Animation = Humanoid:LoadAnimation(Greet)
+                        Animation:Play()
+                        wait(1.6)
+                        for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
+                            if v:IsA("Tool") and v.Name ~= "Combat" or v.Name ~= "[Boombox]" then
+                                game:GetService("Players").LocalPlayer.Character:findFirstChildOfClass'Humanoid':EquipTool(v)
+                            end
+                        end
+                        wait()
+                        game:GetService("VirtualInputManager"):SendKeyEvent(true, "S", false, game)
+                        spawn(function()
+                            while AutoMacroToggle == true do wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                            end
+                        end)
+                        Animation:Stop()
+                        Macro =
+                            game:service'Players'.LocalPlayer.Character:findFirstChildOfClass'Humanoid'.Running:Connect(function()
+                                Animation:Stop()
+                            end)
+                    else
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "AutoMacro";
+                                Text = "AutoMacro: Off";
+                                Duration = 3;
+                            })
+                        end
+                        AutoMacroToggle = false
+                        local Greet = game.ReplicatedStorage.ClientAnimations.Greet
+                        local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+                        local Animation = Humanoid:LoadAnimation(Greet)
+                        Animation:Stop()
+                        game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet"):Remove()
+                        Macro:Disconnect()
+                    end
+                end
+            end
+        end)
+    end
+
+    if Macro == true then
+        game:GetService("UserInputService").InputBegan:Connect(function(input,chatting)
+            if input.UserInputType == Enum.UserInputType.Keyboard then
+                if chatting then IsChatting = true else IsChatting = false end
+                if input.KeyCode == MacroKey then
+                    if IsChatting ~= false then return end
+                    if not MacroToggle then
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "Macro";
+                                Text = "Macro: On";
+                                Duration = 3;
+                            })
+                        end
+                        MacroToggle = true
+                        spawn(function()
+                            while MacroToggle == true do wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                            end
+                        end)
+                    else
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "Macro";
+                                Text = "Macro: Off";
+                                Duration = 3;
+                            })
+                            MacroToggle = false
+                        end
+                    end
+                end
+            end
+        end)
+    end
+end
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "No Jump Cooldowm",
+   Callback = function()
+   if game.PlaceId == 2788229376 then
+    local gmt = getrawmetatable(game)
+    setreadonly(gmt, false)
+    local old = gmt.__newindex
+
+    gmt.__newindex = newcclosure(function(t,i,v)
+        if i == "JumpPower" then
+            return old(t,i,50)
+        end
+        return old(t,i,v)
+    end)
+end
+   end,
+})
